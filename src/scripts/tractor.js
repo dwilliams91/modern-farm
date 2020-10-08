519427fd35286293bac4c50ead4a84fe97dd27f2
Your Github User Name <david.lav.williams@gmail.com>
2020-10-08 11:53:30 -0500

    it now can identify what plant is in ear array

--- src/scripts/tractor.js
+++ src/scripts/tractor.js
@@ -5,15 +5,35 @@
 
 
 //  (e.g. if the food type is "Asparagus", invoke the createAsparagus function to get an asparagus seed).
-export const plantSeeds=(yearsPlanting)=>{
+export const plantSeeds = (yearsPlanting) => {
     //  i will loop through large array
-    let i=0
+    let i = 0
     // j willloop through smaller array
-    let j=0
-    for   (i=0;i<yearsPlanting.length;i++){
-        for (j=0;j<yearsPlanting[i].length;j++){
-            console.log(yearsPlanting[i][j])
+    let j = 0
+    for (i = 0; i < yearsPlanting.length; i++) {
+        for (j = 0; j < yearsPlanting[i].length; j++) {
+            let findCorrectSeed = yearsPlanting[i][j]
+            console.log(findCorrectSeed)
+            if (findCorrectSeed == "Potato") {
+                console.log("Its a potato")
+            }
+            else if (findCorrectSeed == "Soybean") {
+                console.log("Its a soybean")
+            }
+            else if (findCorrectSeed == "Sunflower") {
+                console.log("Its a sunflower")
+            }
+            else if (findCorrectSeed == "Corn") {
+                console.log("Its a corn")
+            }
+            else if (findCorrectSeed == "Wheat") {
+                console.log("Its a wheat")
+            }
+            else if (findCorrectSeed == "Asparagus") {
+                console.log("Its a Asparagus")
+            }
         }
     }
-
 }
+
+
