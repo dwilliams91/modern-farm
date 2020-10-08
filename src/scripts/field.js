ce68732f7f247cb7fdf0e40a06d3ae3bf1bf1831
Your Github User Name <david.lav.williams@gmail.com>
2020-10-08 10:29:12 -0500

    finished the Tilling the Field lesson where I created an array of plants in the fields.

---
+++ src/scripts/field.js
@@ -0,0 +1,17 @@
+let field=[];
+// this function pushes the seed into the field array
+export const addPlant= (seed)=>{
+    field.push(seed)
+    return field
+}
+// this file creates "a copy of the array of plants" as described in the instructions. 
+// I used .slice since that is a command which we used in class
+export const usePlants=(array)=>{
+   let usePlantsArray=[]
+   let i=0
+   for (i=0;i<array.length;i++){
+    usePlantsArray=array.slice(i)
+   }
+   return usePlantsArray
+    
+}
