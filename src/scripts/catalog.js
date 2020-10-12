b678e79a7a529a142f83ba71fff8666809366c5d
Your Github User Name <david.lav.williams@gmail.com>
2020-10-12 09:37:40 -0500

    it is displaying in html the harvested crops

--- src/scripts/catalog.js
+++ src/scripts/catalog.js
@@ -12,12 +12,14 @@ import { field } from './field.js'
 
 export const Catalog=()=>{
     const contentElement=document.querySelector(".messages")
-
     const harvestedCrops=harvestPlants(field)
     let plantString=" "
     for (const plant of harvestedCrops){
+        
+        
+        
         plantString+=plantHTML(plant)
-
+    
     }
 
     contentElement.innerHTML+=plantString
@@ -26,6 +28,14 @@ export const Catalog=()=>{
 }
 
 const plantHTML=(plantObj)=>{
-
-return `<section class=plant>${plantObj.type}</section>`
+let string=" "
+// this part right here creates the html. It also checks to see if it is an
+// array or object since corn is an array but everything else is an object
+if (Array.isArray(plantObj)===true){
+    string=`<section class=plant>${plantObj[0].type}</section>`
+}
+else{
+    string=`<section class=plant>${plantObj.type}</section>`
+}
+return string
 }
